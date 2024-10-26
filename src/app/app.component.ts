d9db120de340659ff2820a1664b2b13b9885987e
kacem <kacemmeherzi52@gmail.com>
2024-10-26 13:59:03 +0100

    experimenting some angular features like template ref , view child decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Time } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,5 +10,38 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app';
+  title = 'WSUUP HOMIE';
+  count = 1 ;
+  inter : any ;
+  color : string = 'red' ;
+@ViewChild("namein") Input !: ElementRef 
+
+  name :string = "kacem"
+  age : number = 23 ;
+  changecolor() :void {
+    this.color = this.Input.nativeElement.value
+  }
+  increament() : void {
+    this.count= this.count+ 1 ;
+  }
+  decreament() : void {
+    this.count = this.count- 1 ;
+  }
+auto () : void {
+  this.inter =  setInterval(()=>{
+    this.count= this.count+1
+  },10)
+  
+}
+stop() : void{
+     clearInterval(this.inter)
+  }
+
+  getname(): string {
+    return this.name ;
+    
+  }
+  changename() : void {
+
+this.name = this.Input.nativeElement.value  }
 }
